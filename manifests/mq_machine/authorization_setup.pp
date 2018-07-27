52bbedd262cfebe1f85272582c5b3fe1b6a0bd53
Bert Hajee <bert.hajee@enterprisemodules.com>
2018-07-27 22:19:28 +0200

    Add support for authentication_info and authorization

--- manifests/mq_machine/authorization_setup.pp
+++ manifests/mq_machine/authorization_setup.pp
@@ -1,8 +1,15 @@
 #++--++
 #--++--
 class ibm_profile::mq_machine::authorization_setup(
+  Hash $authentication_defaults,
+  Hash $authentication_info,
+  Hash $authorization_defaults,
+  Hash $authorization_list,
+
 ) inherits ibm_profile {
-  echo {'MQ Authorizations':
+  echo {'MQ Authorizations & Authentication settings':
     withpath => false,
   }
+  create_resources('mq_authentication_info', $authentication_info, $authentication_defaults )
+  create_resources('mq_authorization', $authorization_list, $authorization_defaults )
 }
