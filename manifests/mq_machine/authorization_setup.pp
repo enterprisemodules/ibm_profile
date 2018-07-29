40ca2f39fe13069b290e43a8ba9c74850e81b856
Bert Hajee <bert.hajee@enterprisemodules.com>
2018-07-29 21:26:20 +0200

    Add support for channel authorization records

--- manifests/mq_machine/authorization_setup.pp
+++ manifests/mq_machine/authorization_setup.pp
@@ -5,11 +5,14 @@ class ibm_profile::mq_machine::authorization_setup(
   Hash $authentication_info,
   Hash $authorization_defaults,
   Hash $authorization_list,
-
+  Hash $channel_authentication_list,
 ) inherits ibm_profile {
-  echo {'MQ Authorizations & Authentication settings':
-    withpath => false,
-  }
+
+  echo {'MQ Authorizations & Authentication settings': withpath => false,}
+  echo {"MQ channel( authentition record(s) ${channel_authentication_list.keys.join(', ')}": withpath => false}
+
   create_resources('mq_authentication_info', $authentication_info, $authentication_defaults )
   create_resources('mq_authorization', $authorization_list, $authorization_defaults )
+  create_resources('mq_channel_authentication', $channel_authentication_list, {})
+
 }
