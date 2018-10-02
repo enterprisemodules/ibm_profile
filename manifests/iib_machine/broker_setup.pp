20daac03295e781a6edd279f91cb288a9a084eb7
Bert Hajee <bert.hajee@enterprisemodules.com>
2018-10-02 09:08:02 +0200

    [iib_machine] Add support for server and broker properties

--- manifests/iib_machine/broker_setup.pp
+++ manifests/iib_machine/broker_setup.pp
@@ -3,10 +3,13 @@
 class ibm_profile::iib_machine::broker_setup(
   Hash $defaults,
   Hash $list,
+  Hash $properties,
 ) inherits ibm_profile {
   echo {"Ensure IIB Brokers(s) ${list.keys.join(', ')}":
     withpath => false,
   }
   $final_defaults = merge($defaults, { 'ensure' => 'present'})
   ensure_resources('iib_broker', $list, $final_defaults)
+
+  ensure_resources('iib_property', $properties)
 }
