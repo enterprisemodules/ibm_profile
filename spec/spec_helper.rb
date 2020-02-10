0fae5319159ded88fad25bda423f1cd2f3a08d61
Bert Hajee <bert.hajee@enterprisemodules.com>
2020-02-10 11:37:06 +0100

    [quality] Fix mq spec’s on Solaris and MQ

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,5 +25,5 @@ end
 
 RSpec.configure do |c|
   c.default_facts = default_facts
-  c.hiera_config = 'hiera.yaml'
+  c.hiera_config = File.expand_path(File.join(__FILE__, '../fixtures/hiera.yaml'))
 end
